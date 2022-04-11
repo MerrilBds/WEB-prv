1960c0be2547bc29a3fa55bda25ee8f4fd056e1a
Mbds <56077@etu.he2b.be>
2022-04-11 18:33:22 +0200

    Js part

---
+++ Parallax/scripts/index.js
@@ -0,0 +1,13 @@
+let first = document.getElementById("first");
+let last = document.getElementById("last");
+let text = document.getElementById("text");
+// dynamic function  that set a position to the elements by an  Add a click event to the window.
+window.addEventListener('scroll'),function(){
+    var value = scrollY;
+     // set the top of the variable 'first'.
+    first.style.top = value * 0.7 + 'px';
+    // set the bottom of the variable 'last'.
+    last.style.bottom = value * 0.5 + 'px';
+    // set the top of the variable 'text.
+    text.style.top = value * 1 + 'px';
+};
